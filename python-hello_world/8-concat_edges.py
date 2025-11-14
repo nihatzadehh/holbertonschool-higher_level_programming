9abdf3e85599d88c96888ff7bd378ba5d9e83e3e
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-11-14 18:45:06 +0000

    n

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:75]+" "+str[str.index("with"):str.index("with")+5]+""+str[:6]
+print(str)
