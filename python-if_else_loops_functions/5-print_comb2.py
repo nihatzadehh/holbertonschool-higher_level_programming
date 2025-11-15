c2acdb4fd91035d677fb150059b5978f9af6163e
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-11-15 08:51:38 +0000

    n

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-print('00', end='')
-for i in range(1, 100):
-    print(", {:02d}".format(i), end='')
-print("")
+for i in range(100):
+    print("{:02d}".format(i), end=', ' if i != 99 else "\n")
