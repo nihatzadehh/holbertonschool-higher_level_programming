0e3d080ebda022a31ce707ad7178276a47adcac4
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-11-15 06:48:01 +0000

    n

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number > 0:
+    lastDig = number%10
+else:
+    lastDig = -int(str(number)[-1])
+    
+if lastDig > 5:
+    case = "and is greater than 5 not 0"
+elif lastDig == 0:
+    case = "and is 0"
+else:
+    case = "and is less than 6 and not 0"
+
+print(f"Last digit of {number} is {lastDig} {case}")
