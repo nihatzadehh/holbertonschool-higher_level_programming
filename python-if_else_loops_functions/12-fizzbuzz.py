6714285608e22350b5779e46f76ca8d420d9b061
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-11-15 12:31:42 +0000

    n

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+for i in range(1, 101):
+    if i % 15 == 0:
+        print("FizzBuzz ")
+    elif i % 5 == 0 and i != 100:
+        print("Buzz ")
+    elif i == 100:
+        print("Buzz\n")
+    elif i % 3 == 0:
+        print("Fizz ")
+    else:
+        print(i, end=" ")
