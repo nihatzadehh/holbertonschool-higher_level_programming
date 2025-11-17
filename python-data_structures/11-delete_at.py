f93ede7eda51efbd49b2a6dfb1ce7c98763daf7d
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-17 22:52:55 +0400

    Update 11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    newlist = list()
     if idx >= 0 and idx < len(my_list):
-        for i in range(len(my_list)):
-            if i != idx:
-                newlist.append(my_list[i])
-        return newlist
+        my_list.remove(my_list[i])
+        return my_list
     else:
         return my_list
