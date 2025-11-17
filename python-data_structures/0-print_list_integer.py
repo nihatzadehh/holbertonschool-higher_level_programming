7fbe9c84be08e0c9dfc8d7a29eb7763c2e54dbc5
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-11-17 16:04:35 +0000

    n

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     def print_list_integer(my_list=[]):
-        print("{:d}\n{:d}\n{:d}\n{:d}\n{:d}".format(my_list[0], my_list[1], my_list[2], my_list[3], my_list[4]))
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
