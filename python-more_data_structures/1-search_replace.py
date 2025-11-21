aca5338110c40546c28599a85558821830195d4f
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-22 01:39:46 +0400

    Create 1-search_replace.py

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+	lastlist = []
+	for i in my_list:
+		if i == search:
+			lastlist.append(search)
+		else:
+			lastlist.append(i)
+	return lastlist
