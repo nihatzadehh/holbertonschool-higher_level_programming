a4988a2f15b2ee2f832654c50143cf6800166349
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-22 01:34:12 +0400

    Create 0-square_matrix_simple.py

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+def square_matrix_simple(matrix=[]):
+    lastmatrix = []
+    for i in matrix:
+        arrayininside = []
+        for j in i:
+            arrayininside.append(j**2)
+        lastmatrix.append(arrayininside)
+        
+    return lastmatrix
