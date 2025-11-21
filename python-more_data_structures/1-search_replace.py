52186f67c8a2b56d726e8a6c5ba954bb383c97b2
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-22 01:47:23 +0400

    Update 1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 def search_replace(my_list, search, replace):
     lastlist = []
     for i in my_list:
-    	if i == search:
-    		lastlist.append(replace)
-    	else:
-    		lastlist.append(i)
+        if i == search:
+            lastlist.append(replace)
+        else:
+            lastlist.append(i)
     return lastlist
