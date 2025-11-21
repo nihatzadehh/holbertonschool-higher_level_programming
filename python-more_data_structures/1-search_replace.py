22142341aa740bbdec519818ae799b56e4f236d7
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-22 01:45:23 +0400

    Update 1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-	lastlist = []
-	for i in my_list:
-		if i == search:
-			lastlist.append(replace)
-		else:
-			lastlist.append(i)
+    lastlist = []
+    for i in my_list:
+	if i == search:
+		lastlist.append(replace)
+	else:
+		lastlist.append(i)
 	return lastlist
