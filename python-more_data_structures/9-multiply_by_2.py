a75436565e778430200c62d73f8b65871eadae63
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-22 10:32:42 +0400

    Update 9-multiply_by_2.py

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(b_dictionary):
+    b_dictionary = b_dictionary
     keys = list(b_dictionary.keys())
     for i in keys:
         b_dictioary[i] = b_dictionary[i]*2
