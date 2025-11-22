a0fe082aefbb164f1ea06fd9c6e5fea5040eee50
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-22 13:15:46 +0400

    Update 4-only_diff_elements.py
    More shorter

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    key = []
-    for i in set_1:
-        if i in set_2:
-            pass
-        else:
-            key.append(i)
-    for i in set_2:
-        if i in set_1:
-            pass
-        else:
-            key.append(i)
-    return key
+    return symmetric_difference(set_1, set_2)
