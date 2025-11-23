74b2aff6a77371d5aec3521219e8582bd29d340e
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-23 08:39:12 +0400

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        print(*my_list[:x], sep='')
-        return x
+        if my_list[x-1]:
+            print(*my_list[:x], sep='')
+            return x
     except:
-        count = 0
+        counter = 0
         for i in my_list:
             print(i, end='')
-            count += 1
-        return count
+            counter += 1
+        return counter
