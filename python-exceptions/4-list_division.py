8a3e58fd240c667c42c5192fd2c71f27d602bcbd
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-23 10:14:55 +0400

    Update 4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -14,4 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print('out of range')
             result.append(0)
         finally:
-            return result
+            continue
+    return result
