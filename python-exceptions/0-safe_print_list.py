849625f6a76f90fd9fbaf30741f294601659c057
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-23 11:26:30 +0400

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             counter += 1
         print('')
         return counter
-    except:
+    except IndexError:
         print('')
         return counter
