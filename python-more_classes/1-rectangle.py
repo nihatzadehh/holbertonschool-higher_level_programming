f97acf6337c393c99f4a66208b6dbcf573d4c75e
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-24 17:14:05 +0400

    Create 1-rectangle.py

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This is my doc"""
+
+
+class Rectangle():
+    """This is mine also"""
+    
+    def __init__(self, width=0, height=0):
+        if isinstance(width, int):
+            if width > 0:
+                self.width(width)
+            else:
+                raise ValueError('width must be >= 0')
+        else:
+            raise TypeError('width must be an integer')
+        
+        if isinstance(height, int):
+            if height > 0:
+                self.height(height)
+            else:
+                raise ValueError('height must be >= 0')
+        else:
+            raise TypeError('height must be an integer')
+        
+    def width(self, value):
+        self._Rectangle__width = value
+    
+    def height(self, value):
+        self._Rectangle__height = value
