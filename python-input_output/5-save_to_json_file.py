0dbcb9a40dea7bf9a1e73c3ec055ebf1ea94a9dd
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-28 21:07:56 +0400

    Create 5-save_to_json_file.py

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Just a doc'''
+
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    '''Just a doc'''
+
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj))
