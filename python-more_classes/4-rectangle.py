30d7694116c5473c1b36ca515500d9b86024ad12
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-28 18:04:22 +0400

    Create 4-rectangle.py

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+''' Just a documentation '''
+
+
+class Rectangle:
+    ''' Just a documentation '''
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError('width must be >= 0')
+        else:
+            raise TypeError('width must be an integer')
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError('height must be >= 0')
+        else:
+            raise TypeError('height must be an integer')
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return (self.__height + self.__width) * 2
+
+    def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return ''
+        a = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                a += '#'
+            if i != self.__height - 1:
+                a += '\n'
+        return a
+
+    def __repr__(self):
+        return 'Rectangle({}, {})'.format(self.width, self.height)
