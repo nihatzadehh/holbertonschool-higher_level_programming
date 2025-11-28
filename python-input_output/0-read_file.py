b60f2825442be32d899f96b5d6db606fd0a28296
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-28 19:49:04 +0400

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+'''Document of the module'''
+
 
 def read_file(filename=""):
+    '''Document of func'''
     with open('UTF8', 'r') as f:
         content = f.read()
         print(f)
