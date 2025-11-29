fcc791665d0c83fb79dea9de103c3fcb44050904
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-30 00:48:24 +0400

    Create 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Just
+a doccument"""
+
+
+class Student:
+    """This is my doccumentation"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        elif len(attrs) == 0:
+            return {}
+        else:
+            dictwillreturn = {}
+            for i in attrs:
+                if self.__dict__.get(i):
+                    dictwillreturn[i] = self.__dict__.get(i)
+            return dictwillreturn
+
+    def reload_from_json(self, json):
+        self.first_name = json['first_name']
+        self.last_name = json['last_name']
+        self.age = json['age']
