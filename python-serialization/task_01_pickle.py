08cc04c3057067b601d63091bf9dc81f8d76fcca
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-30 02:47:36 +0400

    Update task_01_pickle.py

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,31 +1,24 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Just a documentation"""
+
+
 import pickle
+
 
 class CustomObject:
     def __init__(self, name, age, is_student):
         self.name = name
         self.age = age
         self.is_student = is_student
-
     def display(self):
-        print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-        print(f"Is Student: {self.is_student}")
-
+        print('Name: {}'.format(self.name))
+        print('Age: {}'.format(self.age))
+        print('Is Student: {}'.format(self.is_student))
     def serialize(self, filename):
-        try:
-            with open(filename, "wb") as f:
-                pickle.dump(self, f)
-        except (FileNotFoundError, pickle.PickleError, OSError):
-            return None
-
+        with open(filename, 'wb') as f:
+            pickle.dump(self.__dict__, f)
     @classmethod
     def deserialize(cls, filename):
-        try:
-            with open(filename, "rb") as f:
-                obj = pickle.load(f)
-            if isinstance(obj, cls):
-                return obj
-            return None
-        except (FileNotFoundError, pickle.UnpicklingError, EOFError, OSError):
-            return None
+        with open(filename, 'rb') as f:
+            data = f.read()
+            return CustomObject(pickle.loads(data)['name'], pickle.loads(data)['age'], pickle.loads(data)['is_student'])
