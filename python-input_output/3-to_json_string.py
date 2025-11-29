7131807132f12f36ded72164b986ee78808bbcff
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-29 21:44:21 +0400

    Update 3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-'''Just a document'''
-
+"""Just a document"""
 
 
 import json
-def to_json_string(my_obj):
-    '''Just a doc'''
 
+def to_json_string(my_obj):
+    """Just a doc"""
     return json.dumps(my_obj)
