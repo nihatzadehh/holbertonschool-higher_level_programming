c159b78ba953446e5c41d9565bd1cef077b7f4a9
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-29 19:52:34 +0400

    Update 8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """This
 is my doc"""
-import json
 
 def class_to_json(obj):
     """This is my func's docum"""
-    ourstr = json.dumps(obj.__dict__)
-    return json.loads(ourstr)
+    return obj.__dict__
