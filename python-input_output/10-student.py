ffd0fd3de368234d716aeafef0f2f8e67e5ae3ee
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-29 21:42:16 +0400

    Create 10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Just
+a doccument"""
+
+
+class Student:
+    """This is my doccumentation"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if not attrs:
+            return self.__dict__
+        else:
+            dictwillreturn = {}
+            for i in attrs:
+                if self.__dict__.get(i):
+                    dictwillreturn[i] = self.__dict__.get(i)
+            return dictwillreturn
