0629686d23f4826d19eb3df955edc18fbafe57d6
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-30 04:43:57 +0400

    Update 16-shows_by_genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,8 @@
--- asgfsagg
+-- List shows and genres
 SELECT tv_shows.title, tv_genres.name
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+FROM tv_genres
+INNER JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+RIGHT JOIN tv_shows
+    ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_genres.name;
