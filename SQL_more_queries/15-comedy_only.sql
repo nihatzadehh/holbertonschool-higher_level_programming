2299ab47c0210b757c57a42975eb5c647a27ec27
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-30 04:43:32 +0400

    Update 15-comedy_only.sql

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,9 @@
--- JLASHGJKSFHA;
+-- Only Comedy
 SELECT tv_shows.title
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = 'Comedy'
+INNER JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy"
 ORDER BY tv_shows.title ASC;
