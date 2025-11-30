5738f2f6cbb14df58d30d5600d7cb1f743b5d37a
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-30 04:43:19 +0400

    Update 14-my_genres.sql

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,9 @@
--- Join
+-- My genres
 SELECT tv_genres.name
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_shows.id = tv_show_genres.tv_show_id
-WHERE tv_shows.title = 'Dexter'
+INNER JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+    ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = "Dexter"
 ORDER BY tv_genres.name ASC;
