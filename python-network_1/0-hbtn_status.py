9e61b4cdb6238075acdbf665ac162be9458be143
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-12-01 06:06:15 -0500

    0

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This is my doc"""
+
+
+import urllib.request
+
+
+url = 'https://intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    data = response.read()
+    dt = type(data)
+    utfcont = data.decode('utf-8')
+    print('Body response:$')
+    print('\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'.format(data,dt, utfcont))
