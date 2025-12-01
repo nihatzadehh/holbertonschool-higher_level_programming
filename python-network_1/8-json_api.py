217ebf13f12f8dc820a7fa792f00fd2f745e5d87
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-12-01 13:29:36 -0500

    0

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -6,24 +6,17 @@ import sys
 
 if __name__ == '__main__':
     try:
-        # Attempt to get query parameter from command line arguments
         data = {'q': sys.argv[1]}
     except IndexError:
-        # If no argument is passed, set the default value for 'q'
         data = {'q': ''}
 
-    # Send POST request to the server with the query parameter 'q'
     response = requests.post('http://0.0.0.0:5000/search_user', data=data)
 
-    # Check if the response was successful and handle various cases
     try:
-        # Try to parse the response as JSON
         response_json = response.json()
     except ValueError:
-        # If the response isn't valid JSON
         print('Not a valid JSON')
     else:
-        # If the response is valid JSON, handle it accordingly
         if not response_json:
             print('No result')
         else:
