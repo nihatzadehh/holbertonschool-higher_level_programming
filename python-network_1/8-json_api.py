13d86c5b79a337db3b498d75e8b2b0af5f791b69
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-12-01 13:15:04 -0500

    0

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is my doc"""
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    try:
+        data = {'q': sys.argv[1]}
+    except:
+        data = {'q': ''}
+    response = requests.post('http://0.0.0.0:5000/search_user', data=data)
+    if response is None:
+        print('Not a valid JSON')
+    elif len(response) == 0:
+        print('No result')
+    else:
+        print(response.text)
