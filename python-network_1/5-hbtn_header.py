ccca055f9be41f39af135fd19dae799b89cc7124
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-12-01 12:29:28 -0500

    0

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This is my doc"""
 
+if __name__ == '__main__':
+    import requests
+    import sys
 
-import requests
-import sys
 
-
-url = sys.argv[1]
-respond = requests.get(url)
-print(respond.headers.get('X-Request-Id'))
+    url = sys.argv[1]
+    respond = requests.get(url)
+    print(respond.headers.get('X-Request-Id'))
