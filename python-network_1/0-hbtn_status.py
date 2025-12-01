22605d07727d6494dc1b0fc5d7e6e16139d39fb4
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-12-01 06:11:16 -0500

    0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -10,5 +10,5 @@ with urllib.request.urlopen(url) as response:
     data = response.read()
     dt = type(data)
     utfcont = data.decode('utf-8')
-    print('Body response:$')
+    print('Body response:')
     print('\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'.format(dt,data, utfcont))
