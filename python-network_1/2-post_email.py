e0d86412a7d156767c4f793f9d018614e3f87140
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-12-01 07:48:22 -0500

    0

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is my doc"""
+
+
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    mail = sys.argv[2]
+    mail = {'email':mail}
+    mail = urllib.parse.urlencode(mail).encode()
+    req = urllib.request.Request(url, data=mail, method='POST')
+
+    with urllib.request.urlopen(req) as response:
+        result = response.read().decode('utf-8')
+        print(result)
