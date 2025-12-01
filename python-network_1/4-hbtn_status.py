1e00a237a7313ef67f392678b9ef4561f16e04f4
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-12-01 08:28:46 -0500

    0

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ import requests
 if __name__ == '__main__':
     myreq = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
-    print(f'\t- type: {myreq}')
+    print(f'\t- type: {type(myreq.text)}')
     print(f'\t- content: {myreq.text}')
