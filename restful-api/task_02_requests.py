2c85d50c05e7e7705b316c539a56f1ecc7810081
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-12-04 10:52:11 -0500

    n

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -27,11 +27,9 @@ def fetch_and_save_posts():
             }
             for post in fetchedjson
         ]
-    with open('posts.csv', 'w', encoding='utf-8') as f:
-        fieldnames = ['id', 'title', 'body']
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        with open('posts.csv', 'w', encoding='utf-8') as f:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
 
-        writer.writeheader()
-        writer.writerows(filteredjson)
-
-fetch_and_save_posts()
+            writer.writeheader()
+            writer.writerows(filteredjson)
