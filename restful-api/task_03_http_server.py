060d9cb6f22a3c5398b25a74aee051cbe2553994
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-12-06 02:45:14 -0500

    n

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -6,34 +6,29 @@ import http.server
 import json
 
 
-class Handler(http.server.BaseHTTPRequestHandler):
+class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/data':
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-Type', 'text')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+        elif self.path == '/data':
             data = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-Type', 'application/json')
             self.end_headers()
-
-            self.wfile.write(json.dumps(data).encode())
-        elif self.path == '/':
-            message = 'Hello, this is a simple API!'
-            self.send_response(200)
-            self.send_header('Content-type', 'text')
-            self.end_headers()
-
-            self.wfile.write(json.dumps(message).encode())
+            self.wfile.write(json.dumps(data).encode('utf-8'))
         elif self.path == '/info':
             data = {"version": "1.0", "description": "A simple API built with http.server"}
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-Type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps(data).encode())
+            self.wfile.write(json.dumps(data).encode('utf-8'))
         else:
             self.send_response(404)
-            self.send_header('Content-type', 'text')
+            self.send_header('Content-Type', 'text')
             self.end_headers()
-
-            self.wfile.write(json.dumps('404 Not Found').encode())
-
-server = http.server.HTTPServer(('localhost', 8000), Handler)
+            self.wfile.write(b'Endpoint not found')
+server = http.server.HTTPServer(('localhost', 8000), MyHandler)
 server.serve_forever()
