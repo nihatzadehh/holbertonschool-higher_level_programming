bc9c3fff8a84a4e9ac3a590b62faf055e2c76976
Nihat Mammadzada <nihat.mmzade@gmail.com>
2025-12-07 05:30:36 -0500

    n

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 '''users = {"jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"}, "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}}'''
+users = dict()
 
 @app.route('/')
 def home():
